1808dfe66a1a77f4f64ca3e869078e3da9b3ed34
abarnesh01 <abarnesh772@gmail.com>
2026-05-14 20:21:52 +0530

    feat: initialize classifier package and export analysis tools

---
+++ classifier/__init__.py
@@ -0,0 +1,11 @@
+"""SafeWatch Classifier Module."""
+
+from classifier.skeleton_analyzer import SkeletonAnalyzer
+from classifier.velocity_tracker import VelocityTracker
+from classifier.action_classifier import ActionClassifier
+
+__all__ = [
+    "SkeletonAnalyzer",
+    "VelocityTracker",
+    "ActionClassifier"
+]
